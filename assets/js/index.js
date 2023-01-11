f9b4d87963f0bc7a353044d1c6eb13d86b9aceb0
Đỗ Chí Hưng <61616410+dochihung2909@users.noreply.github.com>
2023-01-11 11:27:39 +0700

    Add new catalog

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,13 @@
-const quizApi = 'https://quizapi.io/api/v1/questions?apiKey=a3a6zO32OM5fEK8w72XIpuIRmaizSKMfG2bx8bsj&tags=HTML&limit=20'
 const root = document.getElementById('root') 
+
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+const optionsWrapper = $('.options')
+const options = $$('.option')
+   
+
+
 
 const questions = []
 
@@ -16,61 +24,8 @@ function check(e) {
     } else {
         e.target.style.backgroundColor ='red'
     }
-}
-
-fetch(quizApi)
-    .then((response) =>  { 
-        return response.json()
-    })
-    .then((quizs) =>  {    
-        console.log(quizs)
-        quizs.map((quiz,index) => { 
-            let correctAnswer =[]
-            for (let correct in quiz.correct_answers) {
-                if (quiz.correct_answers[correct] === "true") {
-                    correctAnswer.push(correct.slice(0, 8))
-                }
-            }   
-            questions.push({
-                question: quiz.question,
-                answers: quiz.answers,
-                correct_answer: correctAnswer,
-                explanation: quiz.explanation
-            })
-        })    
-        // questions.map((question,index) => {
-        //     const questionHtml = document.createElement('div')
-        //     questionHtml.className = 'question_wrapper'
-        //     questionHtml.innerHTML = `
-        //     <h3 data-index="${index}" class="question"></h3>`
-        
-        
-        //     questionHtml.querySelector('.question').innerText = question.question
-        
-        //     for (let answer in question.answers) {  
-        //         let answerText = question.answers[answer] 
-        //         if (answerText) {
-        //             const answerHtml = document.createElement('div')
-        //             answerHtml.className = 'answer_wrapper'
-        //             answerHtml.innerHTML = `
-        //             <button onclick="check(event)" data-id="${answer}" class="answer"></button>`
-        //             answerHtml.querySelector('.answer').innerText = answerText
-        //             questionHtml.appendChild(answerHtml)
-        //         }
-        //     } 
-        //     root.appendChild(questionHtml)   
-        // })
-    })
-    .catch((error) => {
-        console.log(error)
-    })  
- 
-    console.log(questions)
-
-const $ = document.querySelector.bind(document)
-const $$ = document.querySelectorAll.bind(document)
-
-const startBtn = $('#start-btn')
+}    
+  
 const questionWrapper = $('.question_wrapper')
 const question = $('.question')
 const answerWrapper = $('.answer_wrapper')  
@@ -78,7 +33,7 @@ const audio = $('#audio')
 
 const app = {
     currentQuestion: 0,
-
+    isSelected: false, 
     showQuestion() {    
         let quest = questions[this.currentQuestion]
         let correctAns = quest.correct_answer
@@ -94,20 +49,23 @@ const app = {
                 `
                 answer.querySelector('.btn').innerText = ansText
                 answer.addEventListener('click', function(event) {
-                    event.target.style.border = '2px solid #fff'
-                    event.target.classList.add('selected')
-                    let ansSelected = event.target.closest('.answer').dataset.ans 
-                    setTimeout(function() {
-                        if (correctAns.includes(ansSelected)) {
-                            event.target.classList.add('right')
-                            
-                        } else {
-                            event.target.classList.add('wrong') 
-                        }
-                    },2000)
-                    setTimeout(function() { 
-                        app.nextQuestion()
-                    },4000)
+                    if (!app.isSelected) {
+                        event.target.style.border = '2px solid #fff'
+                        event.target.classList.add('selected')
+                        let ansSelected = event.target.closest('.answer').dataset.ans 
+                        setTimeout(function() {
+                            if (correctAns.includes(ansSelected)) {
+                                event.target.classList.add('right')
+                                
+                            } else {
+                                event.target.classList.add('wrong') 
+                            }
+                        },2000)
+                        setTimeout(function() { 
+                            app.nextQuestion()
+                        },4000)
+                        app.isSelected = true
+                    } 
                 })  
                 answerWrapper.appendChild(answer)
             } 
@@ -116,16 +74,48 @@ const app = {
     nextQuestion() {
         answerWrapper.innerHTML = ''
         this.currentQuestion++
+        this.isSelected = false
         this.showQuestion()
     },   
-    start() {
-        startBtn.addEventListener('click', function() {
-            startBtn.classList.add('hide')
-            questionWrapper.classList.remove('hide')
-            audio.play()
-            audio.loop = true
-            app.showQuestion()
-        })
+    start() { 
+        options.forEach(option => {
+            option.addEventListener('click', (e) => {
+                optionsWrapper.classList.add('hide')
+                selectOption = e.target.innerText 
+                fetch(`https://quizapi.io/api/v1/questions?apiKey=a3a6zO32OM5fEK8w72XIpuIRmaizSKMfG2bx8bsj&tags=${selectOption}&limit=20`)
+                    .then((response) =>  { 
+                        return response.json()
+                    })
+                    .then((quizs) =>  {    
+                        console.log(quizs)
+                        quizs.map((quiz,index) => { 
+                            let correctAnswer =[]
+                            for (let correct in quiz.correct_answers) {
+                                if (quiz.correct_answers[correct] === "true") {
+                                    correctAnswer.push(correct.slice(0, 8))
+                                }
+                            }   
+                            questions.push({
+                                question: quiz.question,
+                                answers: quiz.answers,
+                                correct_answer: correctAnswer,
+                                explanation: quiz.explanation
+                            })
+                        })     
+                    })
+                    .catch((error) => {
+                        console.log(error)
+                    })     
+                console.log(questions)  
+                startBtn.classList.add('hide')
+                questionWrapper.classList.remove('hide')
+                audio.play()
+                audio.loop = true 
+                setTimeout(() => {
+                    app.showQuestion()
+                }, 1000) 
+            })
+        }); 
     }
 }
 
